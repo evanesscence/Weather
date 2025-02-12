2b384eed10d8845b7ff42f869687733562d058ca
Karina <barakhaeva@inbox.ru>
2025-02-12 18:16:48 +0300

    Создана структура DateHelper, позволяющая форматитировать дату и время

--- Helpers/DateHelper.swift
+++ Helpers/DateHelper.swift
@@ -1,8 +1,48 @@
-//
-//  DateHelper.swift
-//  WeatherAPI
-//
-//  Created by /karisheesh on 10.02.2025.
-//
-
 import Foundation
+
+struct DateHelper {
+    static func extractTime(from dateString: String) -> String {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        if let date = inputFormatter.date(from: dateString) {
+            let outputFormatter = DateFormatter()
+            outputFormatter.dateFormat = "HH:mm"
+            return outputFormatter.string(from: date)
+        }
+        return ""
+    }
+    
+    static func getTimeWithOffset(localTime: String) -> String {
+        let now = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        formatter.timeZone = .current
+        
+        let currentTimeString = formatter.string(from: now)
+        var diff = 0
+        
+        if let first = extractHour(from: localTime),
+           let second = extractHour(from: currentTimeString) {
+            diff = first - second
+        }
+        
+        let calendar = Calendar.current
+        if let newDate = calendar.date(byAdding: .hour, value: diff, to: now) {
+
+            return formatter.string(from: newDate)
+        }
+        return "Ошибка"
+    }
+    
+    static func extractHour(from timeString: String) -> Int? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        
+        if let date = formatter.date(from: timeString) {
+            let calendar = Calendar.current
+            return calendar.component(.hour, from: date)
+        }
+        return nil
+    }
+}
